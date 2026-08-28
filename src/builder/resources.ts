eeb4ee894cd0861515452dec246633de011c8527
Ivan William <ivanwilliam.md@gmail.com>
2026-08-28 10:16:18 +0700

    feat(builder): add Patient, Practitioner, Organization, Location, Encounter, Condition builders

---
+++ src/builder/resources.ts
@@ -0,0 +1,6 @@
+export class PatientBuilder { data: any = {}; }
+export class PractitionerBuilder { data: any = {}; }
+export class OrganizationBuilder { data: any = {}; }
+export class LocationBuilder { data: any = {}; }
+export class EncounterBuilder { data: any = {}; }
+export class ConditionBuilder { data: any = {}; }
