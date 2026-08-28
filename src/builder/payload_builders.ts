ab373525b518cc23b9e2b5c5fc9711ddc4c7d34b
Ivan William <ivanwilliam.md@gmail.com>
2026-08-28 10:33:05 +0700

    feat(builder): add full comprehensive methods for all resource builders in TS

--- src/builder/payload_builders.ts
+++ src/builder/payload_builders.ts
@@ -5,26 +5,58 @@ export class BaseBuilder {
 
 export class PatientBuilder extends BaseBuilder {
   constructor() { super(); this.data.resourceType = 'Patient'; }
+  setNik(nik: string): this {
+    this.data.identifier = [{ system: 'https://fhir.kemkes.go.id/id/nik', value: nik }];
+    return this;
+  }
   setName(name: string): this { this.data.name = [{ use: 'official', text: name }]; return this; }
+  setGender(gender: string): this { this.data.gender = gender; return this; }
+  setBirthDate(date: string): this { this.data.birthDate = date; return this; }
 }
 
 export class PractitionerBuilder extends BaseBuilder {
   constructor() { super(); this.data.resourceType = 'Practitioner'; }
+  setNik(nik: string): this {
+    this.data.identifier = [{ system: 'https://fhir.kemkes.go.id/id/nik', value: nik }];
+    return this;
+  }
   setName(name: string): this { this.data.name = [{ use: 'official', text: name }]; return this; }
 }
 
 export class OrganizationBuilder extends BaseBuilder {
   constructor() { super(); this.data.resourceType = 'Organization'; }
+  setId(id: string): this { this.data.id = id; return this; }
+  setName(name: string): this { this.data.name = name; return this; }
 }
 
 export class LocationBuilder extends BaseBuilder {
   constructor() { super(); this.data.resourceType = 'Location'; }
+  setName(name: string): this { this.data.name = name; return this; }
+  setManagingOrganization(orgRef: string): this {
+    this.data.managingOrganization = { reference: orgRef };
+    return this;
+  }
 }
 
 export class EncounterBuilder extends BaseBuilder {
   constructor() { super(); this.data.resourceType = 'Encounter'; }
+  setStatus(status: string): this { this.data.status = status; return this; }
+  setSubject(ref: string, display: string): this {
+    this.data.subject = { reference: ref, display };
+    return this;
+  }
 }
 
 export class ConditionBuilder extends BaseBuilder {
   constructor() { super(); this.data.resourceType = 'Condition'; }
+  setClinicalStatus(code: string): this {
+    this.data.clinicalStatus = {
+      coding: [{ system: 'http://terminology.hl7.org/CodeSystem/condition-clinical', code }]
+    };
+    return this;
+  }
+  setSubject(ref: string, display: string): this {
+    this.data.subject = { reference: ref, display };
+    return this;
+  }
 }
