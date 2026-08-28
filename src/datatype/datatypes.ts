fa0ce2eb97f9f6f1f74968481c047f67c69b9bdb
Ivan William <ivanwilliam.md@gmail.com>
2026-08-28 10:34:30 +0700

    feat(datatype): add complete set of FHIR DataTypes matching PHP

--- src/datatype/datatypes.ts
+++ src/datatype/datatypes.ts
@@ -1,4 +1,7 @@
 export interface Identifier { system: string; value: string; }
-export interface CodeableConcept { coding?: Coding[]; text?: string; }
-export interface Coding { system?: string; code?: string; display?: string; }
-export interface Reference { reference?: string; display?: string; }
+export interface HumanName { use?: string; text?: string; family?: string; given?: string[]; toArray(): any; }
+export interface Address { line?: string[]; city?: string; district?: string; state?: string; postalCode?: string; country?: string; toArray(): any; }
+export interface ContactPoint { system?: string; value?: string; use?: string; toArray(): any; }
+export interface CodeableConcept { coding?: Coding[]; text?: string; toArray(): any; }
+export interface Coding { system?: string; code?: string; display?: string; toArray(): any; }
+export interface Reference { reference?: string; display?: string; toArray(): any; }
